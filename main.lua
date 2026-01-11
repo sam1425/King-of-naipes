91356d362b1f75032790628e247c26a1a363884f
c0mplex <sadasipa@gmail.com>
2026-01-11 11:52:39 -0500

    working state

--- main.lua
+++ main.lua
@@ -2,7 +2,9 @@ function love.load()
 	require("settings")
 	cam = require("camera")
 	map = require("world")
+	font = require("font")
 	player = require("player")
+	print(map_height, map_width)
 end
 
 function love.update(dt)
@@ -12,6 +14,15 @@ end
 function love.draw()
 	cam:attach()
 	draw_map()
+	DrawFontOnMap()
 	draw_player()
 	cam:detach()
 end
+
+function love.keypressed(key) --, scancode, isrepeat)--
+	if key == "escape" then
+		love.event.quit(1)
+	end
+end
+
+--love.graphics.scale(4)
--- world.lua
+++ world.lua
@@ -7,9 +7,9 @@ map_height = map.height + 16
 map_width = map.width + 16
 
 function draw_map()
-  if map then
-    map:draw(0, 0)
-  end
+	if map then
+		map:draw(0, 0)
+	end
 end
 
 return map
